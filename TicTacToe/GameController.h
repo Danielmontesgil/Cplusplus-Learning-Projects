338dd7999c5bedf5a2c370fbf68f28ed3d7fcb52
Daniel Montes Gil <dmontesgil@gmail.com>
2024-09-23 12:32:37 +0200

    Improved process_input and check_winner implemented

--- TicTacToe/GameController.h
+++ TicTacToe/GameController.h
@@ -4,8 +4,14 @@
 class GameController
 {
 public:
+
+    // GameController methods
     GameController() : player_moves(10,-1){}
-    bool validate_input(char player_input);
+    std::vector<int> get_game_status() { return player_moves; }
+    bool process_input(char player_input, int player);
+    bool check_winner() const;
+    virtual ~GameController() = default;
+    
 private:
     std::vector<int> player_moves;
 };
