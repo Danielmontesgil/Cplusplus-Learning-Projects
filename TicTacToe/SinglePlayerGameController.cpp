7d8abf6916d71da0da3c05a183b2ebfc52ddbd18
Daniel Montes Gil <dmontesgil@gmail.com>
2024-09-24 21:03:38 +0200

    Added SinglePlayerController

---
+++ TicTacToe/SinglePlayerGameController.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include "GameBoardBase.h"
+#include "GameModelBase.h"
+#include "SinglePlayerGameController.h"
+
+void SinglePlayerGameController::init_game()
+{
+    bool finished {false};
+    int player_playing {1};
+
+    system("cls");
+
+    game_board->display_board();
+
+    while(!finished)
+    {
+        char input{};
+        const int player {player_playing % 2 != 0 ? 1 : 2};
+        std::cout << "Player " << (player == 1 ? "1(X)" : "2(O)") << " enter your movement: ";
+        std::cin >> input;
+
+        if(game_model->process_input(input, player))
+        {
+            system("cls");
+            game_board->display_board(game_model->get_game_status());
+            if(player_playing >= 5)
+            {
+                finished = game_model->check_winner();
+            }
+        }
+        
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+        if(finished)
+        {
+            game_board->display_winner(player);
+        }
+        else if(player_playing >= 9)
+        {
+            game_board->display_winner(0);
+            finished = true;
+        }
+
+        player_playing++;
+    }
+}
---
+++ TicTacToe/SinglePlayerGameController.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "GameControllerBase.h"
+
+class SinglePlayerGameController : public GameControllerBase
+{
+public:
+    SinglePlayerGameController(GameModelBase* game_model, GameBoardBase* game_board)
+        : GameControllerBase(game_model, game_board) {}
+    virtual void init_game() override;
+    virtual ~SinglePlayerGameController() override = default;
+};
